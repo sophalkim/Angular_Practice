c9469907d96d85dbf86097c52ec1e036b4587a0a
Sophal Kim <sophal.kim.work@gmail.com>
2014-05-15 10:40:40 -0700

    Routes in javascript, rerouting user directions within the Spa

---
+++ AngularJSDemos_Practice/app/app-practice.js
@@ -0,0 +1,23 @@
+var app = angular.module('customersApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/customers',
+			{
+				controller: 'CustomersController',
+				templateUrl: '/app/partials/customers.html'
+			})
+
+		.when('/customerorders/:customerID',
+			{
+				controller: 'CustomersController',
+				templateUrl: '/app/partials/customerOrders.html'
+			})
+
+		.when('/orders',
+			{
+				controller: 'OrdersController',
+				templateUrl: '/app/partials/orders.html'
+			})
+		.otherwise({ redirectTo: '/customers'});
+})
