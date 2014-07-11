d1ad22e180b6ea6dd5c4b76f956582d5d1c40c00
Sophal Kim <sophal.kim.work@gmail.com>
2014-07-11 15:38:01 -0700

    JSON data

--- AngularJSDemos/app/services/customersService_practice.js
+++ AngularJSDemos/app/services/customersService_practice.js
@@ -30,4 +30,16 @@ app.service('customersService', function() {
 		}
 		return null;
 	};
+
+	var customers = [
+		{
+			id: 1, firstName: 'Lee', lastName: 'Carroll', address: '1234 Anywhere St.', city: 'Phoenix',
+			orders: [
+				{ product: 'Basket', price: 29.99, quantity: 1, orderTotal: 29.99},
+				{ product: 'Yarn', price: 9.99, quantity: 1, orderTotal: 39.96},
+				{ product: 'Needles', price: 5.99, quantity: 1, orderTotal: 5.99}
+			]
+		},
+		
+	]
 })
