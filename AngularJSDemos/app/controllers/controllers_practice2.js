0036d85c52f9b9822e3128f83a309f0fc35a0284
Sophal Kim <sophal.kim.work@gmail.com>
2014-07-20 23:36:25 -0700

    insertCustomer will add new Customers into customersService, then clear.

--- AngularJSDemos/app/controllers/controllers_practice2.js
+++ AngularJSDemos/app/controllers/controllers_practice2.js
@@ -4,4 +4,14 @@ app.controller('CustomersController', function ($scope, customersService) {
 	function init() {
 		$scope.customers = customersService.getCustomers();
 	}
+
+	$scope.insertCustomer = function() {
+		var firstName = $scope.newCustomer.firstName;
+		var lastName = #scope.newCustomer.lastName;
+		var city = $scope.newCustomer.city;
+		customersService.insertCustomer(firstName, lastName, city);
+		$scope.newCustomer.firstName = '';
+		$scope.newCustomer.lastName = '';
+		$scope.newCustomer.city = '';
+	};
 })
