a0a092451d0ec34b03fdc1cb7ff992b6a1454b35
Sophal Kim <sophal.kim.work@gmail.com>
2014-07-21 15:50:06 -0700

    different controllers for different uses

--- AngularJSDemos/app/controllers/controllers_practice2.js
+++ AngularJSDemos/app/controllers/controllers_practice2.js
@@ -41,3 +41,39 @@ app.controller('OrdersController', function ($scope, customersService) {
 		$scope.customers = customersService.getCustomers();
 	}
 });
+
+app.controller('NavbarController', function ($scope, $location) {
+	$scope.getClass = function (path) {
+		if ($location.path().substr(0, path.length) == path) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+});
+
+app.controller('OrderChildController', function ($scope) {
+	$scope.orderby = 'product';
+	$scope.reverse = false;
+	$scope.ordersTotal = 0.00;
+
+	init();
+
+	function init() {
+		if ($scope.customer && $scope.customer.orders) {
+			var total = 0.00;
+			for (var i =0; i < $scope.customer.orders.length; i++) {
+				var order = $scope.customer.orders[i];
+				total += order.orderTotal;
+			}
+			$scope.ordersTotal = total;
+		}
+	}
+
+	$scope.setOrder = function (orderby) {
+		if (orderby === $scope.orderby) {
+			$scope.reverse = !$scope.reverse;
+		}
+		$scope.orderby = orderby;
+	};
+});
