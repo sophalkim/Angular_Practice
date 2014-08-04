ff9afacc937259ad27a26ac0c9f86d50b1eb815a
Sophal Kim <sophal.kim.work@gmail.com>
2014-08-04 09:07:10 -0700

    Only one function has argument

---
+++ AngularJSDemos/app/controllers/controllers_practice4.js
@@ -0,0 +1,21 @@
+app.controller('CustomersController', function ($scope, customersService)) {
+	init();
+	function init() {
+		$scope.customers = customersService.getCustomers();
+	}
+
+	$scope.insertCustomer = function() {
+		var firstName = $scope.newCustomer.firstName;
+		var lastName = $scope.newCustomer.lastName;
+		var city = $scope.newCustomer.city;
+		customersService.insertCustomer(firstName, lastName, city);
+		$scope.newCustomer.firstName = '';
+		$scope.newCustomer.lastName = '';
+		$scope.newCustomer.city = '';
+	};
+
+	$scope.deleteCustomer = function (id) {
+		customersService.deleteCustomer(id);
+	};
+});
+
