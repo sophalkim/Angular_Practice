c02aae2a433fa392c75702d010df6c81330dedd9
Sophal Kim <sophal.kim.work@gmail.com>
2014-08-05 10:34:54 -0700

    Finish controllers for customer management program

--- AngularJSDemos/app/controllers/controllers_practice4.js
+++ AngularJSDemos/app/controllers/controllers_practice4.js
@@ -31,3 +31,47 @@ app.controller('CustomerOrdersController', function ($scope, $routeParams, custo
 		}
 	}
 });
+
+app.controller('OrdersController', function ($scope, customersService) {
+	$scope.customers = [];
+	init();
+
+	function init() {
+		$scope.customers = customersService.getCustomers();
+	}
+});
+
+app.controller('NavbarController', function ($scope, $location) {
+	$scope.getClass = function (path) {
+		if ($location.path().substr(0, path.length) == path) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+});
+
+app.controller('OrderChildController', function ($scope,) {
+	$scope.orderby = 'product';
+	$scope.reverse = false;
+	$scope.ordersTotal = 0.00;
+	init();
+
+	function init() {
+		if ($scope.customer && $scope.customer.orders) {
+			var total = 0.00;
+			for (var i = 0; i < $scope.customer.orders.length; i++) {
+				var order = $scope.customer.orders[i];
+				total += order.orderTotal;
+			}
+			$scope.orderTotal = total;
+		}
+	}
+
+	$scope.setOrder = function (orderby) {
+		if (orderby === $scope.orderby) {
+			$scope.reverse = !$scope.reverse;
+		}
+		$scope.orderby = orderby;
+	};
+});
