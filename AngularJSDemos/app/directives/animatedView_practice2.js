f197f2f1ad070e13559b23ac1a9698dd80fe9521
Sophal Kim <sophal.kim.work@gmail.com>
2014-08-10 10:31:26 -0700

    Directive for Animated View Transition

---
+++ AngularJSDemos/app/directives/animatedView_practice2.js
@@ -0,0 +1,17 @@
+app.directive('animatedView', ['$route', '$anchorScroll', '$compile', '$controller', function ($route, $anchorScroll, $compile, $controller) {
+	return {
+		restrict: 'ECA',
+		terminal: true,
+		link: function (scope, element, attr) {
+			var lastScope,
+				onloadExp = attr.onload || '',
+				defaults = { duration: 500, viewEnterAnimation: 'slideLeft', viewExitAnimation: 'fadeOut', slideAmount: 50, disabled: false}, 
+				locals,
+				template,
+				options = scope.$eval(attr.animations);
+			angular.extend(defaults, options);
+			scope.$on('$routeChangeSuccess', update);
+			update();
+		}
+	}
+}])
