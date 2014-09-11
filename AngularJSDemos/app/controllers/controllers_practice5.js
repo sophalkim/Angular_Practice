efeeab7367302cae3841e0492f56b2126e2bdb87
Sophal Kim <sophal.kim.work@gmail.com>
2014-09-11 08:34:15 -0700

    getClass takes argument and return if it matches location

--- AngularJSDemos/app/controllers/controllers_practice5.js
+++ AngularJSDemos/app/controllers/controllers_practice5.js
@@ -42,3 +42,14 @@ app.controller('OrdersController', function ($scope, customersService) {
 		$scope.customers = customersService.getCustomers();
 	}
 });
+
+app.controller('NavbarController', function ($scope, $location) {
+	$scope.getClass = function (path) {
+		if ($location.path().substr(0, path.length) == path) {
+			return true;
+		} else {
+			return false;
+		}
+	};
+});
+
