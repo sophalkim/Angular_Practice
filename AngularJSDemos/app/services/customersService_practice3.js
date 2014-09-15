ba3ec15cb399296e993a5a82b364430bb210ea33
sophalkim <sophal.kim.work@gmail.com>
2014-09-14 21:37:18 -0700

    Testing out GitEye

--- AngularJSDemos/app/services/customersService_practice3.js
+++ AngularJSDemos/app/services/customersService_practice3.js
@@ -2,4 +2,23 @@ app.service('customerService', function() {
 	this.getCustomers = function() {
 		return customers;
 	};
+
+	this.insertCustomer = function (firstName, lastName, city) {
+		var topID = customers.length + 1;
+		customers.push({
+			id: topID,
+			firstName: firstName,
+			lastName: lastName,
+			city: city
+		});
+	};
+
+	this.deleteCustomer = function (id) {
+		for (var i = customers.length - 1; i >= 0; i--) {
+			if (customers[i].id === id) {
+				customers.splice(i, 1);
+				break;
+			}
+		}
+	};
 });
